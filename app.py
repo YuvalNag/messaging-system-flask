01b88fd709631c8dd6ff27a601b158b0fc02298b
YuvalNag <33052299+YuvalNag@users.noreply.github.com>
2020-06-02 18:27:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.message import Message, MessageList
 
-
+from db import db
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=3600)
@@ -23,13 +24,18 @@ app.secret_key = 'yuval'
 api = Api(app)
 
 
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Message, '/message/', '/message/<int:message_id>')
 api.add_resource(MessageList, '/messages')
 api.add_resource(UserRegister, '/register')
 
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 if __name__ == '__main__':
+    db.init_app(app)
     app.run(port=5000, debug=True)
